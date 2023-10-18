476055e75d3b2169354d9f56550ba575a6b70e8c
jeannepetitdangeon <petitdangeonjeanne@gmail.com>
2023-10-18 13:56:08 +0200

    Update Pycodetransfo.py

--- Pycodetransfo.py
+++ Pycodetransfo.py
@@ -6,6 +6,13 @@ import os
 dat = pd.read_csv('C:/Users/epcmic/OneDrive/Documents/GitHub/Transformer/Challenge/data_tweet_sample_challenge.csv')
 
 # Keeping important columns, ID, time, content, author id et username, number of followers,  number of likes, number of rt, language, country code
+# • id: Unique identifier for each tweet
+# • created at: Time at which the tweet was posted
+# • text: The actual content of the tweet
+# • author.id: Unique identifier for each user
+# • public metrics.like count: Number of likes
+# • public metrics.retweet count: Number of retweets
+# • label: Country code ISO-2
 
 var = dat.loc[:,["id","created_at", "text", "author.id", "author.username", "author.public_metrics.followers_count","public_metrics.like_count","public_metrics.retweet_count","lang", "label"]]
 
@@ -21,3 +28,40 @@ print("Number of missing values in 'public_metrics.like_count':", var['public_me
 print("Number of missing values in 'public_metrics.retweet_count':", var['public_metrics.retweet_count'].isna().sum())
 print("Number of missing values in 'lang':", var['lang'].isna().sum())
 print("Number of missing values in 'label':", var['label'].isna().sum())
+
+
+# Data Pre-processing
+# Text cleaning on the tweet content.
+
+
+# Visualize the data to understand the distribution of tweets over time, 
+# by newspaper, and by engagement metrics (likes, retweets).
+
+
+
+# Extract hashtags from the tweet text
+
+
+
+
+
+
+# Data Analysis
+# Apply Name Entity Recognition (NER) to identify key entities in the tweets.
+
+
+
+# Perform Sentiment Analysis to evaluate newspaper sentiment towards this technologies.
+
+
+
+
+
+# Translate non-English tweets into English.
+
+
+
+
+# Utilize Zero-Shot Classification to categorize tweets into predefined or dynamically identified topics.
+
+
